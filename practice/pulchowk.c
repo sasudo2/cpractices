dd91ac98861e0374a51431603652983a8f97ad27
sasudo2 <sanjayashrestha777@gmail.com>
2023-09-21 08:34:43 +0545

    pattern as asked by adarsha

---
+++ practice/pulchowk.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char c[8] = "pulchowk";
+    for(int i = 7; i>=0; i--){
+        for(int j = 0; j<i; j++){
+            printf(" ");
+        }
+        for(int k = i; k<8; k++){
+            printf("%c", c[k]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
