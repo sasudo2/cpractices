4844cc378fb1b18ab56427a413a045e749ecae15
sasudo2 <sanjayashrestha777@gmail.com>
2023-09-21 11:41:21 +0545

    copying data from one file to another

---
+++ .history/practice/copy_20230921114010.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    FILE *fp1, *fp2;
+    fp1 = fopen("append.txt", "r");
+    fp2 = fopen("copy.txt", "w");
+    char ch;
+    while((ch =fgetc(fp1))!=EOF){
+        fputc(ch, fp2);
+    }
+    fclose(fp1);
+    fclose(fp2);
+    return 0;
+
+}
